8db258975854445d9de5bddd2655b04d93b06664
rodrigo gomes <rodgomesc@gmail.com>
2020-05-30 17:01:04 -0300

    chore(initial for notes container): notes container

---
+++ src/containers/Notes/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Notes: React.FC = () => {
+  return <View />;
+};
+
+export default Notes;
