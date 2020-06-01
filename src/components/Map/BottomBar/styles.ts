fa53b3e0b72dc2271d689d925d1303cdc2bac6bb
rodrigo gomes <rodgomesc@gmail.com>
2020-06-01 16:34:27 -0300

    style(layout change): little layout change

--- src/components/Map/BottomBar/styles.ts
+++ src/components/Map/BottomBar/styles.ts
@@ -4,8 +4,8 @@ export const Container = styled.View`
   flex-direction: row;
   height: 100px;
   border: 2px solid rgba(137, 255, 226, 0.6);
-  border-top-left-radius: 60px;
-  border-top-right-radius: 60px;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
   background: #ecf0f3;
   margin-top: auto;
   align-items: center;
