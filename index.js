8bbb682982ea123d9d2e38665cb577a531b41198
rodrigo gomes <rodgomesc@gmail.com>
2020-06-03 15:57:19 -0300

    feat(bumo): bump release

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import App from './src/App';
 import { name as appName } from './app.json';
 import { YellowBox } from 'react-native';
 
+
 YellowBox.ignoreWarnings(['Called stopObserving with existing subscriptions']);
 AppRegistry.registerComponent(appName, () => App);
